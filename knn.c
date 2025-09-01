fdb429d32f887268070a61aa5d2e258e5d016ccb
Aarav kumar <aaravkumar7479@gmail.com>
2025-09-01 21:09:00 +0530

    initial commit add a file knn.c

---
+++ knn.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+typedef struct Data{
+    float x;
+    float y;
+    int label;
+}Data;
+
+
+int main(){
+
+
+
+
+
+
+
+    return 0;
+} 
